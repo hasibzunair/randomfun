82163e73325e3b3fa26bd37b9bf50642da721467
Hasib Zunair <hasibzunair@gmail.com>
2018-07-16 08:22:14 +0600

    print statement updated

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -1,4 +1,3 @@
-#from numpy import *
 import numpy as np
 
 # sum of squared errors
@@ -56,13 +55,14 @@ def run():
     #print(b)
     #print(m)
 
-    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_given_points(initial_b,
-                                                                                                            initial_m,
-                                                                                                  points)))
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m,
+                                                                              compute_error_for_given_points(initial_b,
+                                                                                                             initial_m,
+                                                                                                             points)))
     print("Running...")
     [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
     print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m,
-                                                                compute_error_for_given_points(b, m, points)))
+                                                                      compute_error_for_given_points(b, m, points)))
 
 
 if __name__ == '__main__':
